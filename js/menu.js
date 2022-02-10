f3105761d05363617d23079b598c29b19f40d09a
SerhiiRizvan <94913378+SerhiiRizvan@users.noreply.github.com>
2022-02-10 15:59:07 +0300

    Добавляет меню

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,6 @@
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
   function toggleModal() {
-    refs.menu.classList.toggle('is-open');
+    refs.menu.classList.toggle('is-hidden');
   }
 })();
